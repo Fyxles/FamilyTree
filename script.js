bd064d8cbe1336ccccf92bb3f48d7812de86d3d7
anonymous <kyleedbert@gmail.com>
2026-08-01 11:13:19 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const supabase = window.supabase.createClient(
+  "https://rmvplzymnhqrlnbvkiip.supabase.co/rest/v1/",
+  "sb_publishable_IXcUdMTRAG6L0sEqMDTP5w_0F1WtSYn"
+);
+
+console.log("Connected to Supabase!");
