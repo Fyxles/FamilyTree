bbb853d8e6d05c75b1737a7027afa48d9becc90d
anonymous <kyleedbert@gmail.com>
2026-08-01 11:33:23 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const supabase = window.supabase.createClient(
-  "https://rmvplzymnhqrlnbvkiip.supabase.co/rest/v1/",
-  "sb_publishable_IXcUdMTRAG6L0sEqMDTP5w_0F1WtSYn"
-);
-
-console.log("Connected to Supabase!");
